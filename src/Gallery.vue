fa050d0914bc159714381f971e19f0b35ecc27da
Jonathan Treffler <mail@jonathan-treffler.de>
2020-09-02 18:17:21 +0200

    added the initial code

---
+++ src/Gallery.vue
@@ -0,0 +1,329 @@
+<template>
+	<div class="gallery">
+		<div class="zoom_blur_background" @click="closeZoom" />
+		<picture @click="closeZoom">
+			<source :srcset="currentWebpSrc" type="image/webp">
+			<img class="zoomed_image" :src="currentJpgSrc" type="image/jpg" alt="">
+		</picture>
+		<span class="zoomed_image_source" />
+		<div class="gallery_control_container">
+			<img class="gallery_control" alt="" src="~/assets/arrow_back.svg" @click="scroll(-1)">
+		</div>
+		<div class="pictureContainer">
+			<slot />
+		</div>
+		<div class="gallery_control_container">
+			<img class="gallery_control" alt="" src="~/assets/arrow_forward.svg" @click="scroll(1)">
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	props: [],
+	data() {
+		return {
+			currentJpgSrc: '',
+			currentWebpSrc: '',
+			activeId: 0,
+		};
+	},
+	mounted() {
+		this.$on('zoomPicture', function(id) {
+			const pictureContainer = this.$el.querySelector('.pictureContainer');
+
+			const pictures = pictureContainer.children;
+
+			let picture = this.getPictureElementById(id);
+
+			this.$el.classList.add('zoom');
+
+			const pos = picture.getBoundingClientRect();
+			const height = pos.height;
+			const width = pos.width;
+
+			const zoomedImage = this.$el.querySelector('.zoomed_image');
+			zoomedImage.style.display = '';
+
+			this.currentWebpSrc = picture.getAttribute('data-webp-src');
+			this.currentJpgSrc = picture.getAttribute('data-jpg-src');
+
+			function setSource(thisEl, picture) {
+				return new Promise(function(resolve, reject) {
+					const source = picture.getAttribute('data-source');
+					if (source) {
+						thisEl.querySelector('.zoomed_image_source').textContent = 'Quelle: ' + source;
+					} else {
+						thisEl.querySelector('.zoomed_image_source').textContent = '';
+					}
+
+					resolve();
+				});
+			}
+
+			function setToOrgiginalPos(pos, width, height) {
+				return new Promise(function(resolve, reject) {
+					zoomedImage.style.top = pos.top + 'px';
+					zoomedImage.style.left = pos.left + 'px';
+					zoomedImage.style.width = width + 'px';
+					zoomedImage.style.height = height + 'px';
+					zoomedImage.style.maxWidth = width + 'px';
+					zoomedImage.style.maxHeight = height + 'px';
+
+					resolve();
+				});
+			}
+
+			function activateBlurBackground(thisEl) {
+				return new Promise(function(resolve, reject) {
+					const zoomBlurBackground = thisEl.querySelector('.zoom_blur_background');
+					zoomBlurBackground.style.display = '';
+					zoomBlurBackground.style.opacity = 0.8;
+
+					resolve();
+				});
+			}
+
+			function centerImagePX() {
+				return new Promise(function(resolve, reject) {
+					const htmlBounds = document.getElementsByTagName('html')[0].getBoundingClientRect();
+					const zoomedImageBounds = zoomedImage.getBoundingClientRect();
+
+					zoomedImage.classList.add('topLeftTransition');
+					zoomedImage.style.top = (htmlBounds.height - zoomedImageBounds.height) / 2 + 'px';
+					zoomedImage.style.left = (htmlBounds.width - zoomedImageBounds.width) / 2 + 'px';
+
+					setTimeout(function() {
+						zoomedImage.classList.remove('topLeftTransition');
+						resolve();
+					}, 500);
+				});
+			}
+
+			function centerImagePC() {
+				return new Promise(function(resolve, reject) {
+					zoomedImage.style.top = '50%';
+					zoomedImage.style.left = '50%';
+					zoomedImage.style.transform = 'translate(-50%, -50%)';
+
+					resolve();
+				});
+			}
+
+			function extendPX() {
+				return new Promise(function(resolve, reject) {
+					zoomedImage.style.width = '';
+					zoomedImage.style.height = '';
+
+					const htmlBounds = document.getElementsByTagName('html')[0].getBoundingClientRect();
+
+					zoomedImage.classList.add('maxWidthHeightTransition');
+					zoomedImage.style.maxWidth = htmlBounds.width + 'px';
+					zoomedImage.style.maxHeight = htmlBounds.height + 'px';
+
+					setTimeout(function() {
+						zoomedImage.classList.remove('maxWidthHeightTransition');
+						resolve();
+					}, 500);
+				});
+			}
+
+			function extendPC() {
+				return new Promise(function(resolve, reject) {
+					zoomedImage.style.maxWidth = '';
+					zoomedImage.style.maxWidth = '100%';
+					zoomedImage.style.maxHeight = '';
+					zoomedImage.style.maxHeight = '100%';
+
+					resolve();
+				});
+			}
+
+			function activateClosing(thisEl) {
+				const zoomBlurBackground = thisEl.querySelector('.zoom_blur_background');
+				return new Promise(function(resolve, reject) {
+					zoomBlurBackground.classList.add('closeable');
+					resolve();
+				});
+			}
+
+			setSource(this.$el, picture)
+				.then(setToOrgiginalPos(pos, width, height))
+				.then(activateBlurBackground(this.$el))
+				.then(centerImagePX)
+				.then(centerImagePC)
+				.then(extendPX)
+				.then(extendPC)
+				.then(activateClosing(this.$el));
+		});
+	},
+	methods: {
+		getPictureElementById(id) {
+			const pictureContainer = this.$el.querySelector('.pictureContainer');
+			const pictures = pictureContainer.children;
+
+			return pictures[id];
+		},
+		scroll(direction) {
+			const pictureContainer = this.$el.querySelector('.pictureContainer');
+			const pictures = pictureContainer.children;
+
+			const newCurrentId = this.activeId + direction;
+
+			let newCurrentPicture = this.getPictureElementById(newCurrentId);
+
+			if (newCurrentPicture) {
+				this.scrollToElement({
+					container: pictureContainer,
+					element: newCurrentPicture,
+					duration: 500,
+				});
+
+				this.activeId = newCurrentId;
+			}
+		},
+		closeZoom() {
+			const zoomBlurBackground = this.$el.querySelector('.zoom_blur_background');
+
+			if (zoomBlurBackground.classList.contains('closeable')) {
+				const zoomedImage = this.$el.querySelector('.zoomed_image');
+
+				const self = this;
+				zoomBlurBackground.style.opacity = 0;
+
+				setTimeout(function() {
+					self.$el.classList.remove('zoom');
+					zoomBlurBackground.style.display = 'none';
+				}, 400);
+
+				zoomedImage.style.display = 'none';
+				zoomedImage.removeAttribute('style');
+				this.currentWebpSrc = '';
+				this.currentJpgSrc = '';
+
+				zoomBlurBackground.classList.remove('closeable');
+			}
+		},
+		getOffset(el) {
+			let _x = 0;
+			let _y = 0;
+			while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+				_x += el.offsetLeft;
+				_y += el.offsetTop;
+				el = el.offsetParent;
+			}
+			return { top: _y, left: _x, };
+		},
+		scrollToElement(options) {
+			const scrollLeftStart = options.container.scrollLeft;
+			const containerOffsetLeft = this.getOffset(options.container).left;
+			const scrollLeftEnd = options.element.offsetLeft - containerOffsetLeft;
+
+			let start;
+
+			function easeInOutCubic(x) {
+				return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+			}
+
+			function step(timestamp) {
+				if (start === undefined) {
+					start = timestamp;
+				}
+
+				const elapsed = timestamp - start;
+				const progress = Math.min(elapsed / options.duration, 1);
+
+				const currentLeft = scrollLeftStart + ((scrollLeftEnd - scrollLeftStart) * easeInOutCubic(progress));
+
+				options.container.scrollLeft = currentLeft;
+
+				if (elapsed < options.duration) {
+					window.requestAnimationFrame(step);
+				}
+			}
+
+			window.requestAnimationFrame(step);
+		},
+	},
+};
+</script>
+<style>
+	.gallery {
+		width: 90%;
+		display: flex;
+		flex-flow: row;
+		flex-wrap: nowrap;
+		margin: auto;
+		margin-bottom: 5px;
+	}
+	.gallery_control_container {
+		width: 10%;
+		vertical-align: middle;
+		display: flex;
+	}
+	.gallery_control {
+		width: 100%;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+		object-fit: contain;
+	}
+	.pictureContainer {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+		justify-content: flex-start;
+		width: 80%;
+		overflow-y: scroll;
+		scrollbar-width: none;
+		-ms-overflow-style: none;
+	}
+	.pictureContainer::-webkit-scrollbar {
+			width: 0;
+			height: 0;
+	}
+	.zoom_blur_background {
+		background-color: black;
+		z-index: -1;
+		top: 0px;
+		left: 0px;
+		display: block;
+		position: fixed;
+		width: 100%;
+		height: 100%;
+		opacity: 0;
+		transition: opacity 0.4s;
+	}
+	.gallery.zoom .zoom_blur_background {
+		z-index: 2000;
+	}
+	.gallery.zoom .zoomed_image_source {
+		display: block;
+		position: fixed;
+		bottom: 15px;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+	.zoomed_image_source {
+		display: none;
+		z-index: 2002;
+		color: white;
+	}
+	.zoomed_image {
+		position: fixed;
+		z-index: 2002;
+	}
+	.zoomed_image.topLeftTransition {
+		transition: left 0.5s linear, top 0.5s linear;
+	}
+	.zoomed_image.maxWidthHeightTransition {
+		transition: max-width 0.5s linear, max-height 0.5s linear;
+	}
+	@media (pointer:none), (pointer:coarse) {
+		.pictureContainer {
+			width: 100%;
+		}
+		.gallery_control_container {
+			display: none;
+		}
+	}
+</style>
---
+++ src/GalleryImage.vue
@@ -0,0 +1,59 @@
+<template>
+	<picture
+		class="picture"
+		:data-source="source"
+		:data-jpg-src="require('~/assets/pictures/' + src)"
+		:data-webp-src="require('~/assets/pictures/' + src + '?webp')"
+		@click="$parent.$emit('zoomPicture', getId())"
+	>
+		<source :srcSet="require('~/assets/pictures/' + src + '?format=webp&resize&sizes[]=200&sizes[]=300&sizes[]=600&sizes[]=700').srcSet" type="image/webp">
+		<source :srcSet="require('~/assets/pictures/' + src + '?format=jpg&resize&sizes[]=200&sizes[]=300&sizes[]=600&sizes[]=700').srcSet" type="image/jpg">
+		<img :src="require('~/assets/pictures/' + src)" :alt="alt">
+	</picture>
+</template>
+<script>
+export default {
+	props: {
+		src: {
+			type: String,
+			default: '',
+		},
+		alt: {
+			type: String,
+			default: '',
+		},
+		source: {
+			type: String,
+			default: '',
+		},
+	},
+	methods: {
+		getId: function() {
+			console.log(this.$el);
+			let pictures = this.$el.parentNode.children;
+			for(let id in pictures) {
+				console.log(id);
+				console.log(pictures[id]);
+				if(pictures[id] == this.$el) {
+					return id;
+				}
+			}
+		},
+	},
+};
+</script>
+<style>
+	.picture, .picture img {
+		height: 400px;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+	}
+	#zoomed_image {
+		position: fixed;
+		z-index: 2001;
+	}
+	.picture:not(:last-child) {
+		margin-right: 7px;
+	}
+</style>
